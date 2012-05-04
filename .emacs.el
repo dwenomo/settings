c3050257e6f6b082416b11466081b531893ddb19
dwenomo <dwenomo@gmail.com>
2012-05-04 22:53:46 +0900

    add .emacs.el

---
+++ .emacs.el
@@ -0,0 +1,29 @@
+;; package.el
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(require 'package)
+;;リポジトリにMarmaladeを追加
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;インストールするディレクトリを指定
+(setq package-user-dir (concat user-emacs-directory "vendor/elpa"))
+;;インストールしたパッケージにロードパスを通してロードする
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(fringe-mode 0 nil (fringe))
+ '(scroll-bar-mode (quote right))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "VL ゴシック")))))
+
+;; mozc
+(require 'mozc)
+(setq default-input-method "japanese-mozc")
+;(define-key global-map [zenkaku-hankaku] 'toggle-input-method)
