af6465d6d7742d0a340955ce69b6d587296a78d6
dwenomo <dwenomo@gmail.com>
2012-05-05 03:29:33 +0900

    add gosh setting

--- .emacs.el
+++ .emacs.el
@@ -27,3 +27,9 @@
 (require 'mozc)
 (setq default-input-method "japanese-mozc")
 ;(define-key global-map [zenkaku-hankaku] 'toggle-input-method)
+
+(setq process-coding-system-alist
+      (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
+(setq scheme-program-name "gosh -i")
+(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
