ef99229767eb1f93f063ec07db6965b17aa45bee
dwenomo <dwenomo@gmail.com>
2012-05-06 02:02:21 +0900

    change .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -1,38 +1,15 @@
-;; package.el
-(add-to-list 'load-path "~/.emacs.d/elisp")
-(require 'package)
-;;リポジトリにMarmaladeを追加
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-;;インストールするディレクトリを指定
-(setq package-user-dir (concat user-emacs-directory "vendor/elpa"))
-;;インストールしたパッケージにロードパスを通してロードする
-(package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(fringe-mode 0 nil (fringe))
- '(scroll-bar-mode (quote right))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "VL ゴシック")))))
+(setq load-path (cons "~/.emacs.d/elisp" load-path))
+(setq load-path (cons "~/.emacs.d/auto-install" load-path))
 
-;; mozc
-(require 'mozc)
-(setq default-input-method "japanese-mozc")
-;(define-key global-map [zenkaku-hankaku] 'toggle-input-method)
+;; まず、install-elisp のコマンドを使える様にします。
+(require 'install-elisp)
+;; 次に、Elisp ファイルをインストールする場所を指定します。
+(setq install-elisp-repository-directory "~/.emacs.d/elisp/")
 
-(setq process-coding-system-alist
-      (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
-(setq scheme-program-name "gosh -i")
-(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
-(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
+(require 'auto-install)
+(setq auto-install-directory "~/.emacs.d/auto-install/")
+(auto-install-update-emacswiki-package-name t)
+(auto-install-compatibility-setup)             ; 互換性確保
 
-(require 'auto-complete)
-(global-auto-complete-mode t)
+(require 'anything-startup)
+
